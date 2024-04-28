8cc745d370ee309ee280f366abf9e26177cca3cb
shikhharsiingh <shikhharsiingh@gmail.com>
2024-04-27 22:09:00 -0700

    modify script

---
+++ scripts/run_ace.sh
@@ -0,0 +1,1 @@
+python -m torch.distributed.launch --nproc_per_node=4 --master_port 28301 main.py --arch swin_base --batch_size_per_gpu 8 --data_path /Users/shikhharsiingh/JLiangLab/datasets/nih_xray14/images --output_dir /Users/shikhharsiingh/JLiangLab/ACE/outputs/local --saveckp_freq 10 --cfg /scratch/ssiingh/JLiangLab/ACE/swin_configs/swinv2_base_patch4_window16_256.yaml
